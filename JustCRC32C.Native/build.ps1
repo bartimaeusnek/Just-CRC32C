1286bdfff22e674f48f311f9673c352e1b248a71
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2023-07-29 11:41:31 +0200

    Update build.ps1

--- JustCRC32C.Native/build.ps1
+++ JustCRC32C.Native/build.ps1
@@ -1,6 +1,7 @@
 cd $PSScriptRoot
+if (-not (Test-Path -Path "..\JustCRC32C\JustCRC32C_Native_x64.dll")) {
 if (-not (Test-Path -Path "$PSScriptRoot\fasm\FASM.EXE")){
-    $tmp = New-TemporaryFile | Rename-Item -NewName { $_ -replace 'tmp$', 'zip' } �PassThru
+    $tmp = New-TemporaryFile | Rename-Item -NewName { $_ -replace 'tmp$', 'zip' } -PassThru
     Invoke-WebRequest -OutFile $tmp https://flatassembler.net/fasmw17331.zip
     $tmp | Expand-Archive -DestinationPath .\fasm -Force
     $tmp | Remove-Item
@@ -10,3 +11,4 @@ $env:INCLUDE="$PSScriptRoot\fasm\INCLUDE"
 &.\fasm\fasm.exe .\JustCRC32C_Native_x86.ASM
 Copy-Item .\JustCRC32C_Native_x64.dll ..\JustCRC32C\JustCRC32C_Native_x64.dll
 Copy-Item .\JustCRC32C_Native_x86.dll ..\JustCRC32C\JustCRC32C_Native_x86.dll
+}
